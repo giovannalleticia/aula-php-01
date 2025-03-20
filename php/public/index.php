e8a36cb38ec1df21591778e46c580f6ef988028e
efigenioluiz <hanzou0012@gmail.com>
2025-03-20 09:59:44 -0300

    feat: add simple forms with local memory

--- php/public/index.php
+++ php/public/index.php
@@ -8,9 +8,28 @@
     <title>PHP!!</title>
 </head>
 <body>
-    <h1>Isso é um H1</h1>
-    <?php $nome='Luiz';
+    <h1>Aula 2 PHP</h1>
 
-    ?>
+
+    <table>
+        <tr>
+        <th>Nome</th>
+        <th>Idade</th>
+        </tr>
+        <?php
+            // echo var_dump($pessoas);
+            if (isset($pessoas) && is_array($pessoas)) {
+                foreach($pessoas as $pessoa) {
+                    echo "<tr>";
+                    echo "<td>".$pessoa['nome']."</td>";
+                    echo "<td>".$pessoa['idade']."</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr><td colspan='2'>Nenhuma pessoa cadastrada.</td></tr>";
+            }
+        ?>
+
+    </table>
 </body>
 </html>
--- php/scripts-php/aula01.php
+++ php/scripts-php/aula01.php
@@ -1,62 +1,6 @@
 <?php
-
-function soma($a, $b) {
-    return $a + $b;
-}
-
-if (soma(1, 2) == 3) {
-    echo 'Teste ok';
-}elseif (soma(1, 2) == 4) {
-    echo 'Teste ok';
-}
-else {
-    echo 'Teste não ok';
-
-}
-
-class Pessoa {
-    public $nome;
-    protected $idade;
-    private $sexo;
-
-    function falar(){
-        echo 'Olá, meu nome é ' . $this->nome;
-    }
-}
-
-$nome= 'Prof Luiz';
-$bool = true;
-$idade = 30;
-$preco = 12.50;
-$nulo = null;
-
-$pessoa = new Pessoa();
-
-$fruits= ['maçã', 'banana', 'laranja'];
-
-$objcts = Array(
-    0 => 'maçã',
-    'fruta' => 'maçã',
-    0 => 'maçã',
-);
-
-echo $objcts['fruta'];
-
-
-echo $fruits[0];
-
-for ($i = 0; $i < count($fruits); $i++) {
-    echo $fruits[$i] . '<br>';
-}
-foreach ($fruits as $fruit) {
-    echo $fruit . '<br>';
-}
-echo $pessoa->nome = $nome;
-
-$pessoa->falar();
-
-echo $nome;
-
-
+$pessoas = [
+    ['nome' => 'Alice', 'idade' => 30],
+];
 
 ?>
